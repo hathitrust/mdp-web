a9f3ac85bcb401b4f37fb73bf8a227b62cc306b3
Roger Espinosa <roger@umich.edu>
2011-05-23 16:26:22 -0400

    stop PT feedback form from repeat notifications

--- js/feedbackForm.js
+++ js/feedbackForm.js
@@ -1,9 +1,73 @@
 var displayPTFeedback = function() {
     
     var DEFAULT_EMAIL_VALUE = "[Your email address]";
+
+    var $div = $("#mdpFeedbackForm");
+    var $frm = $div.find("form");
+    var frm = $frm.get(0);
+
+    var dialog = new Boxy($div, {
+        show : false,
+        modal: true,
+        draggable : false,
+        closeable : false,
+        clone : false,
+        unloadOnHide: false
+    });
+    
+    if ( ! $frm.data('initialized') ) {
+      // bind the events in the closure in order to share the DEFAULT_EMAIL_VALUE variable
+      $("input[type=submit], #mdpFBcancel", $frm).click(function(e) {
+          var clicked = this;
+          e.preventDefault();
+          
+          // get a version of the dialog that 
+          // has the most current visibility settings
+          var dialog = Boxy.get(this);
+
+          if ( $(this).attr('id') == "mdpFBcancel" ) {
+              dialog.hide();
+              return false;
+          }
+
+          var isEmpty = 1;
+
+          var postData = {};
+
+          for (var i=0; i < frm.length; i++)
+          {
+              if ((frm.elements[i].type == 'checkbox' || frm.elements[i].type == 'radio')
+                  && frm.elements[i].checked) {
+                  isEmpty = 0;
+                  postData[frm.elements[i].name] = frm.elements[i].value;
+              }
+              else if ((frm.elements[i].type == 'text' || frm.elements[i].type == 'textarea')
+                  && (frm.elements[i].value.length > 0)
+                  && (frm.elements[i].value != DEFAULT_EMAIL_VALUE)) {
+                  isEmpty = 0;
+                  postData[frm.elements[i].name] = frm.elements[i].value;
+              }
+              else if ( frm.elements[i].type == 'hidden' ) {
+                postData[frm.elements[i].name] = frm.elements[i].value;
+              }
+          }
+
+          if (isEmpty == 1) {
+              $(frm).find("#emptyFBError").show();
+          } else {
+              // post the form
+              var href = $(frm).attr('action');
+
+              $.post(href, postData);
+              dialog.hide();
+          }
+
+          return false;
+      });
+      $frm.data('initialized', true);
+    }
     
     // empty out the form
-    var frm = $("#mdpFeedbackForm form").get(0);
     $("#emptyFBError").hide();
     
     for (var i=0; i < frm.length; i++)
@@ -22,59 +86,6 @@ var displayPTFeedback = function() {
         }
     }
     
-    // $("#mdpFeedbackForm").boxy();
-    var dialog = new Boxy("#mdpFeedbackForm", {
-        show : false,
-        modal: true,
-        draggable : false,
-        closeable : false,
-        clone : false,
-        unloadOnHide: false
-    });
-    dialog.getContent().find("input[type=submit], #mdpFBcancel").click(function(e) {
-        var clicked = this;
-        e.preventDefault();
-        
-        if ( $(this).attr('id') == "mdpFBcancel" ) {
-            dialog.hide();
-            return false;
-        }
-        
-        var isEmpty = 1;
-        var frm = $(this).parents("form").get(0); // document.getElementById("mdpFBform");
-        
-        var postData = {};
-
-        for (var i=0; i < frm.length; i++)
-        {
-            if ((frm.elements[i].type == 'checkbox' || frm.elements[i].type == 'radio')
-                && frm.elements[i].checked) {
-                isEmpty = 0;
-                postData[frm.elements[i].name] = frm.elements[i].value;
-            }
-            else if ((frm.elements[i].type == 'text' || frm.elements[i].type == 'textarea')
-                && (frm.elements[i].value.length > 0)
-                && (frm.elements[i].value != DEFAULT_EMAIL_VALUE)) {
-                isEmpty = 0;
-                postData[frm.elements[i].name] = frm.elements[i].value;
-            }
-            else if ( frm.elements[i].type == 'hidden' ) {
-              postData[frm.elements[i].name] = frm.elements[i].value;
-            }
-        }
-
-        if (isEmpty == 1) {
-            $(frm).find("#emptyFBError").show();
-        } else {
-            // post the form
-            var href = $(frm).attr('action');
-            
-            $.post(href, postData);
-            dialog.hide();
-        }
-
-        return false;
-    })
     dialog.show();
 
     // initPTFeedback();
